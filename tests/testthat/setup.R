d4975f072e586ec1e613a363b7c81c7e0648189d
Rico Derks <r.j.e.derks@lumc.nl>
2021-03-24 16:34:33 +0100

    Added tests for plot_sims().

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,9 +1,13 @@
 library(testthat)
 library(eisaCIM)
 
-my_test_file <- test_path("ref", "mammalian_cell_ref_original_Hilic_pos_F2_eisa_sim.mzML")
+my_test_file <- test_path("data", "mammalian_cell_ref_original_Hilic_pos_F2_eisa_sim.mzML")
 
 sim_names <- c("241", "152", "120", "74")
 sim_ids <- c(14, 12, 6, 25)
 
 raw_data <- read_files(files = my_test_file)
+
+sim_data <- extract_sim_data(data = raw_data,
+                             sim_names = sim_names,
+                             sim_ids = sim_ids)
---
+++ tests/testthat/test-plot_sims.R
@@ -0,0 +1,28 @@
+# create plot for testing
+p <- plot_sims(sim_data,
+               rt_line = 5.3,
+               title = "SIM's of cystine")
+
+wrong_sim_data <- sim_data
+colnames(wrong_sim_data) <- c("rta", "intensity", "sim")
+
+test_that("does the plot look ok", {
+  vdiffr::expect_doppelganger("SIM graph", p)
+})
+
+test_that("output correct class",{
+  expect_equal(class(p), c("gg", "ggplot"))
+})
+
+test_that("check inputs", {
+  expect_error(plot_sims(raw_data,
+                         rt_line = 5.3,
+                         title = "SIM's of cystine"),
+               "'sim_data' is not a data frame!")
+
+  expect_error(plot_sims(wrong_sim_data,
+                         rt_line = 5.3,
+                         title = "SIM's of cystine"),
+               "'sim_data' doesn't contain all the correct columns!")
+
+})
