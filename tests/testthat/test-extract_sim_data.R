4f8d82b927714a98db74af6a01639104ca2da0b3
Rico Derks <r.j.e.derks@lumc.nl>
2021-03-24 15:58:39 +0100

    Additional tests for extract_sim_data().

--- tests/testthat/test-extract_sim_data.R
+++ tests/testthat/test-extract_sim_data.R
@@ -18,3 +18,18 @@ test_that("input data is not MChromatograms", {
                                 sim_ids = sim_ids[1]),
                "'sim_names' and 'sim_ids' need to have the same length!!")
 })
+
+test_that("correct column names", {
+  expect_named(extract_sim_data(data = raw_data,
+                                sim_names = sim_names,
+                                sim_ids = sim_ids),
+               c("rt", "intensity", "sim"))
+})
+
+test_that("correct dimension output", {
+  my_dim <- dim(extract_sim_data(data = raw_data,
+                                 sim_names = sim_names,
+                                 sim_ids = sim_ids))
+
+  expect_equal(my_dim, c(4996, 3))
+})
