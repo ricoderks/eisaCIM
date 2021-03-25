73511a37cb59126abc6f6a742720a743ba33f172
Rico Derks <r.j.e.derks@lumc.nl>
2021-03-25 14:51:45 +0100

    Added extra test to read_files().

--- tests/testthat/test-read_files.R
+++ tests/testthat/test-read_files.R
@@ -1,10 +1,13 @@
-test_that("correct S4 class output", {
+test_that("output check", {
+  my_dim <- dim(read_files(files = my_test_file))
+
+  expect_equal(my_dim, c(32, 1))
+
   expect_s4_class(read_files(files = my_test_file),
                   "MChromatograms")
 })
 
-test_that("output correct dimensions", {
-  my_dim <- dim(read_files(files = my_test_file))
-
-  expect_equal(my_dim, c(32, 1))
+test_that("input check", {
+  expect_error(read_files(files = 123),
+               "'files' should be a character vector!")
 })
